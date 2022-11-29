811efb6d46b1a5ef2657fa68122e87a7adecae7c
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-11-29 08:33:29 +0000

    added content to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-
+<section class="w-[100wv] h-[300px] bg-[url('../assets/images/codeBackground.png')] bg-cover pt-[60px]">
+    <div class="w-[80%] h-[60%] bg-[#111111]/80 mx-auto rounded-md">
+        <p class="text-center text-[70px] font-['Impact'] pt-[20px] text-gray-200">About me</p>
+    </div>
+</section>
+<section class="w-[100wv] bg-[#1E1E1E] text-center">
+<p class="font-bold md:text-[50px] text-[30px] text-gray-200 pt-10 font-['roman']">Hi, I'm Mateusz Kroplewski.</p>
+<div class="md:w-[400px] w-[90%] mx-auto">
+    <img src="@/assets/images/me.jpg" alt="" class="w-[100%] h-[200px] rounded-md mt-10">
+</div>
+<p class="text-gray-300 text-[20px] w-[700px] text-left mx-auto mt-10 leading-loose">
+A front-end developer from the UK. I have a passion for web programming which originated from game development.
+I love creating challenging and fun projects and looking back how it was an empty repo at the beginning. 
+I am currently studying web programming at University of Huddersfield. Apart from coding I love going to the gym and lifting weights.
+</p>
+</section>
 </template>
 
 
