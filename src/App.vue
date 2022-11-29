eb6fa9b1640bc111d0ff776f0c61322349782bc9
Mateusz <kroplewskimateusz@gmail.com>
2022-11-29 06:39:34 +0000

    project images added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <Nav></Nav>
-  <HomeView></HomeView>
+  <!-- <HomeView></HomeView> -->
+  <AboutView></AboutView>
 </template>
 
 <script setup>
 import Nav from "@/components/nav.vue";
 import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+
 </script>
