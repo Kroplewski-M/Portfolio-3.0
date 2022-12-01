fdbe0ad14d69fcdde3edb75367834a3cac31a7ae
Mateusz <kroplewskimateusz@gmail.com>
2022-12-01 10:28:42 +0000

    starting to render projects

--- src/views/Skills&ProjectsView.vue
+++ src/views/Skills&ProjectsView.vue
@@ -4,11 +4,11 @@
           <p class="text-center md:text-[70px] text-[35px] pt-10 md:pt-0 font-['Impact'] pt-[20px] text-gray-200">Skills&Projects</p>
         </div>
     </section>
-    <section class="w-[100vw] h-[70vh] bg-[#1E1E1E] pt-10">
+    <section class="w-[100vw] pb-10 bg-[#1E1E1E] pt-10">
         <div class="md:flex mx-auto md:w-[850px]">
             <img src="@/assets/images/skillsImage.jpg" alt="" class="md:w-[500px] md:h-[500px] w-[200px] h-[200px] mx-auto md:mx-0 rounded-md">
             <div class="ml-5">
-                <p class="font-semibold text-gray-200 text-[50px] underline">Skills</p>
+                <p class="font-semibold text-gray-200 text-[50px] underline md:-mt-[20px]">Skills</p>
                 <p class="text-gray-400 md:w-[400px] leading-loose">Since the beginning of my web dev journey I have learned a lot of things which help me solve day to day challenges. 
                 I proficiently know the main three pillars of web dev: HTML, CSS and JS. Also, 
                 I use tailwind CSS in majority of my project so I am very familiar and comfortable with it. 
@@ -24,7 +24,25 @@
                     <li>Supabase</li>
                 </ul>
             </div>
-
+        </div>
+    </section>
+    <section class="w-[100vw] pb-10 bg-[#222222] pt-10">
+        <div class="md:w-[80%] w-[90%] h-[90%] bg-[#111111] mx-auto rounded-md">
+            <p class="text-gray-200 underline text-[50px] font-semibold text-center">Projects</p>
+            <div class="flex flex-wrap mx-auto xl:w-[1200px] w-[310px] mt-10">
+                <div v-for="project in projectStore.projects" class="md:w-[300px] w-[250px] h-[400px] bg-[#1E1E1E] rounded-md mb-5 mx-auto md:ml-10 md:mr-0">
+                   
+                </div>
+            </div>
         </div>
     </section>
 </template>
+
+<script setup>
+import {useProjectStore} from '@/stores/projectStore.js';
+
+
+const projectStore = useProjectStore();
+console.log(projectStore.projects);
+
+</script>
