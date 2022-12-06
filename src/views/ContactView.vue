14c6680e2a7e672c9588b9c73b3f825426563e99
Mateusz <kroplewskimateusz@gmail.com>
2022-12-06 16:22:55 +0000

    adding socials

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,7 +4,7 @@
           <p class="text-center md:text-[70px] text-[35px] pt-10 md:pt-5 font-['Impact'] pt-[20px] text-gray-200">Contact</p>
         </div>
     </section>
-    <section class="w-[100vw] max-w-[1500px] h-[70vh] bg-[#1E1E1E] relative md:flex md:place-content-center">
+    <section class="w-[100vw] h-[70vh] bg-[#1E1E1E] relative md:flex md:place-content-center">
       <div class="w-[300px] text-center pt-10 mx-auto md:mx-0">
         <h1 class="text-[30px] text-gray-200 font-['Impact']">Send me a message  →</h1>
         <h1 class="text-[25px] text-gray-400 font-['Impact'] mt-5"> Or reach me here  ↓</h1>
